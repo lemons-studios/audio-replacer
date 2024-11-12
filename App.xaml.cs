992890578d3bdd8340434f76571562686a6d1480
Shob3r <andrewpinter23@gmail.com>
2024-11-11 22:03:50 -0800

    Fix gitignore destroying builds I think I'll use my OWN gitignore next time thank you very much I also readded the 30 gazillion unused usings temporarily for testing, will remove them if app still works without them

--- App.xaml.cs
+++ App.xaml.cs
@@ -1,4 +1,19 @@
 ﻿using Microsoft.UI.Xaml;
+using Microsoft.UI.Xaml.Controls;
+using Microsoft.UI.Xaml.Controls.Primitives;
+using Microsoft.UI.Xaml.Data;
+using Microsoft.UI.Xaml.Input;
+using Microsoft.UI.Xaml.Media;
+using Microsoft.UI.Xaml.Navigation;
+using Microsoft.UI.Xaml.Shapes;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.ApplicationModel;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
 
 namespace AudioReplacer2
 {
@@ -6,13 +21,15 @@ namespace AudioReplacer2
     {
         public App()
         {
-            this.InitializeComponent();
+            InitializeComponent();
         }
-        protected override void OnLaunched(Microsoft.UI.Xaml.LaunchActivatedEventArgs args)
+
+        protected override void OnLaunched(LaunchActivatedEventArgs args)
         {
             m_window = new MainWindow();
             m_window.Activate();
         }
+
         private Window m_window;
     }
 }
