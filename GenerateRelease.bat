04416bad0df1730781c3378cd35fe4a0f6502128
Shob3r <andrewpinter23@gmail.com>
2025-03-16 00:17:13 -0700

    Add release channels so basically roughly 330mb of the app is one dll that can only be used on nvidia gpus and even then, it would only be for audio processing. there will now be a "stable" channel that uses slower speech to text processing for those who don't want to use speech to text (still optional like always) or those who do not have an nvidia gpu and a "stable-nvidia" channel that includes the massive dll. file size reductions gaming

--- GenerateRelease.bat
+++ GenerateRelease.bat
@@ -5,36 +5,41 @@ if "%~1"=="" (
     exit /b 1
 )
 
-:: Check if dotnet is installed
 where dotnet >nul 2>&1 || (
     echo The .NET SDK is not installed.
     echo Please install the .NET sdk from https://dotnet.microsoft.com/en-us/download
     exit /b 1
 )
 
-:: Ensure that Labs-Windows source is added to project
-echo (1/5) Adding Windows Community Toolkit Labs NuGet package source
+echo (1/8) Adding Windows Community Toolkit Labs NuGet package source
 dotnet nuget add source https://pkgs.dev.azure.com/dotnet/CommunityToolkit/_packaging/CommunityToolkit-Labs/nuget/v3/index.json --name Labs-Windows > nul 2> nul
 
-:: Update vpk before build
-echo (2/5) Updating Velopack CLI
+echo (2/8) Updating Velopack CLI
 dotnet tool update -g vpk > nul 2> nul
 
-:: Publish release build before packaging application
-echo (3/5) Creating Release Build
-dotnet publish -c Release --self-contained -r win-x64 -o .\
+echo (3/8) Creating Release Build
+dotnet publish -c Release --self-contained -r win-x64 -o .\Publish
+del /s /q Publish\WinUIEditor.pdb > nul 2> nul
 
 :: For whatever reason, dotnet publish doesn't copy over the assets folder. Let's copy it over manually here
-echo (4/5) Copying Assets
+echo (4/8) Copying Assets
 mkdir Publish\Assets\ > nul 2> nul
 copy /y Assets Publish\Assets\ > nul 2> nul
 
-:: Build app installer with vpk
-echo (5/5) Building app installer with vpk...
-vpk pack -u AudioReplacer -v %~1 -p .\Publish -e AudioReplacer.exe --splashImage .\Assets\SplashScreen.gif -i .\Assets\AppIcon.ico --noPortable --skipVeloAppCheck --signSkipDll --packTitle "Audio Replacer" --packAuthors "Lemon Studios" --delta BestSize || (
+echo (5/8) Building App (stable-nvidia)
+vpk pack -u AudioReplacer -v %~1 -p .\Publish -e AudioReplacer.exe --splashImage .\Assets\SplashScreen.gif -i .\Assets\AppIcon.ico --noPortable --skipVeloAppCheck --signSkipDll --packTitle "Audio Replacer" --packAuthors "Lemon Studios" --delta BestSize --channel stable-nvidia || (
     echo Error: vpk build process failed.
     exit /b 1
 )
 
-echo Build complete!
+echo (7/8) Building App (stable)
+del /s /q Publish\runtimes\cuda > nul 2> nul
+vpk pack -u AudioReplacer -v %~1 -p .\Publish -e AudioReplacer.exe --splashImage .\Assets\SplashScreen.gif -i .\Assets\AppIcon.ico --noPortable --skipVeloAppCheck --signSkipDll --packTitle "Audio Replacer" --packAuthors "Lemon Studios" --delta BestSize --channel stable || (
+    echo Error: vpk build process failed.
+    exit /b 1
+)
+
+echo (8/8) Cleanup
+del /s /q Publish\ > nul 2> nul
+
 exit /b 0
