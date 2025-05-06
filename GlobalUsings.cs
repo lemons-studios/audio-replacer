68847741c94c53a57abd97bceffc874df1fdd43a
Shob3r <andrewpinter23@gmail.com>
2025-05-06 10:25:53 -0700

    Whar

--- GlobalUsings.cs
+++
@@ -1,9 +0,0 @@
-﻿// Global using directives
-
-global using System;
-global using System.IO;
-global using System.Threading.Tasks;
-global using AudioReplacer.Generic;
-global using AudioReplacer.Util;
-global using Microsoft.UI.Xaml;
-global using System.Collections.Generic;
