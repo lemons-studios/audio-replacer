7d0eb407044c8e38d9db145d9d469143fb9ce91f
Shob3r <andrewpinter23@gmail.com>
2025-06-02 17:16:39 -0700

    add ffmpeg.wasm binaries to project I need them for local wasm execution since I want to keep the app offline. ffmpeg.wasm is licensed MIT, which means credit isn't needed, but I'll make sure to add it in some credits page later on.

--- src/util/WhisperUtils.ts
+++ src/util/WhisperUtils.ts
@@ -2,18 +2,18 @@ import { invoke } from "@tauri-apps/api/core";
 import { whisperLocation } from "../generic/AppProperties";
 import { DownloadFile } from "./NetworkUtils";
 
-export async function transcribeFile(inputFile: string): Promise<string> {
+export async function transcribeFile(filePath: string): Promise<string> {
     return new Promise((resolve) => {
         invoke("transcribe_file", {
-            path: inputFile,
-            modelPath: whisperLocation
+            path: filePath,
+            model_path: whisperLocation
         }).then((res) => {
-            resolve(res as string);
+            resolve(res as string)
         })
     })
 }
 
 export async function downloadTranscriptionData() {
-    const url = "https://huggingface.co/ggerganov/whisper.cpp/resolve/main/ggml-base.en-q5_1.bin"; // Unlikely to change
+    const url = "https://huggingface.co/ggerganov/whisper.cpp/resolve/main/ggml-base.en-q5_1.bin";
     DownloadFile(url, whisperLocation);
 }
