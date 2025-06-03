ebf25d619acba37c312cd2cdaddc499368bb4fab
Shob3r <andrewpinter23@gmail.com>
2025-06-03 13:16:33 -0700

    possibly add full ffmpeg integration

--- src/util/FFMpegManager.ts
+++ src/util/FFMpegManager.ts
@@ -1,11 +1,49 @@
 import { Command } from "@tauri-apps/plugin-shell";
 import { resolveResource } from "@tauri-apps/api/path";
-import { readFile } from "@tauri-apps/plugin-fs";
-import { fetchFile, toBlobURL } from '@ffmpeg/util';
+import { copyFile, readFile, readTextFile, remove } from "@tauri-apps/plugin-fs";
+import { FFmpeg } from '@ffmpeg/ffmpeg';
+
+let ffmpeg: FFmpeg | null = null;
 
 export async function applyFfmpegFilter(input: string, output: string, filterList: string, overwrite: boolean) {
-    const load = async() => {
-        const wasmPath = await resolveResource("binaries/ffmpeg.wasm");
-        const ffmpegCore = await resolveResource("binaries/ffmpeg-core.js");
+    await initializeFfmpeg(); 
+    await ffmpeg?.writeFile(output, input);
+
+    await ffmpeg?.exec(["-i", input, `-af ${filterList}`, output]);
+
+    if(overwrite) {
+        remove(input);
+        copyFile(output, input);
+        remove(output);
     }
 }
+
+async function initializeFfmpeg() {
+    if(ffmpeg) return;
+
+    const coreUrl = await getFfmpegCore();
+    const wasmUrl = await getFfmpegWasm();
+
+    ffmpeg = new FFmpeg();
+    await ffmpeg.load({
+        coreURL: coreUrl,
+        wasmURL: wasmUrl
+    });
+    
+}
+
+async function getFfmpegCore(): Promise<string> {
+    const jsPath = await resolveResource("binaries/ffmpeg-core.js");
+    const jsText = await readTextFile(jsPath);
+    const jsBlob = new Blob([jsText], {type: "application/javascript"});
+    return URL.createObjectURL(jsBlob)
+}
+
+async function getFfmpegWasm() : Promise<string> {
+    const wasmPath = await resolveResource("binaries/ffmpeg.wasm");
+    const wasmBytes = await readFile(wasmPath);
+    const wasmBlob = new Blob([new Uint8Array(wasmBytes)], {type: "application/wasm"});
+    return URL.createObjectURL(wasmBlob)
+}
+
+
--- src/util/NetworkUtils.ts
+++ src/util/NetworkUtils.ts
@@ -1,7 +1,6 @@
 import { fetch } from "@tauri-apps/plugin-http";
 import { download } from "@tauri-apps/plugin-upload";
 
-
 // This method is most likely going to only be used by one or two functions of the app, and mainly intended for fetching the release notes from the GitHub repo
 export async function JsonNetDataFromTag(url: string, tag: string): Promise<string | null> {
     return new Promise((resolve) => {
