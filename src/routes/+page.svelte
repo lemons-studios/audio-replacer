b2984e695b07b7f6c98a2ec5e61e1a51c7a296a7
Shob3r <andrewpinter23@gmail.com>
2025-06-12 12:58:35 -0700

    more frontend work, i'm just gonna remake v4 ui and go from there

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,30 +1,38 @@
 <script lang="ts">
-  import { initializeValues } from "../generic/AppProperties";
   import { onMount } from "svelte";
-  import { JsonNetDataFromTag } from "../util/NetworkUtils";
+  import { JsonNetDataFromTag } from "../Util/NetworkUtils";
   import SvelteMarkdown from "@humanspeak/svelte-markdown";
-  import { json } from "@sveltejs/kit";
 
-  let markdown: string = `No recent changes`;
+  let markdown = $state("# No Changes Found");
 
   onMount(async() => {
     const url = "https://api.github.com/repos/lemons-studios/audio-replacer/releases/latest";
     const releaseData = await JsonNetDataFromTag(url, "body") ?? "# Error uh oh";
-    markdown = JSON.parse(releaseData).body;
+    markdown = releaseData;
   })
 
 </script>
 
-
-
 <div class="grid grid-cols-2 gap-5 h-full">
   <div class="grid grid-rows-2 gap-5">
     <div class="dark:bg-title-card-dark rounded-xl p-5 drop-shadow-lg">
-        <h1 class="text-center align-top text-4xl"><b>Load Project</b></h1>
-
+        <h1 class="text-center align-top text-4xl m-10"><b>Load Project</b></h1>
+        <div class="flex justify-center gap-10">
+          <div class="dark:bg-secondary-card-dark">
+            <h1>Last Project</h1>
+          </div>
+          
+          <div>
+            <h1>New Project</h1>
+          </div>
+        </div>
     </div>
     <div class="dark:bg-title-card-dark rounded-xl p-5 drop-shadow-lg">
-        <h1 class="text-center align-top text-4xl"><b>Stats</b></h1>
+        <h1 class="text-center align-top text-4xl mb-10"><b>Stats</b></h1>
+        <p>Time Spent With Project Open</p>
+        <p>Files Transcribed</p>
+        <p>Files Skipped</p>
+        <p>Recordings cancelled/discarded</p>
     </div>
   </div>
   <div class="rounded-xl dark:bg-title-card-dark p-5 drop-shadow-lg">
