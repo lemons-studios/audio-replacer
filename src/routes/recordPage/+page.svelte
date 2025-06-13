5a8fc8bf2459fbffb7dd79b833c2e5821c09fa7c
Shob3r <andrewpinter23@gmail.com>
2025-06-13 14:47:47 -0700

    some work on record page

--- src/routes/recordPage/+page.svelte
+++ src/routes/recordPage/+page.svelte
@@ -1,10 +1,16 @@
 <script lang="ts">
-    import { onMount } from 'svelte';
-  import { setDetails } from '../../Util/DiscordRpc';
+  import { onMount } from "svelte";
+  import { setDetails } from "../../Util/DiscordRpc";
+  import { stat } from "@tauri-apps/plugin-fs";
 
-    onMount(async() => {
-        await setDetails("Recording")
-    })
+  // I LOVE RUNES
+  let idleState = $state(true);
+  let recordingState = $state(false);
+  let reviewingState = $state(false);
+
+  onMount(async () => {
+    await setDetails("Recording");
+  });
 </script>
 
 <h1>Record Page</h1>
