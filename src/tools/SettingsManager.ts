0bed1434a230a3d1fc9c888358335edc115c557e
Shob3r <andrewpinter23@gmail.com>
2025-09-24 10:54:26 -0700

    update deps + make settings work

--- src/tools/SettingsManager.ts
+++ src/tools/SettingsManager.ts
@@ -1,6 +1,7 @@
 import { resolveResource } from "@tauri-apps/api/path";
 import { readTextFile, writeTextFile } from "@tauri-apps/plugin-fs";
 import { debounce } from "./OsTools";
+import { info, error } from "@tauri-apps/plugin-log";
 
 let settingsJson: any;
 let loaded: boolean = false;
@@ -10,8 +11,10 @@ async function loadSettings() {
         const path = await resolveResource("resources/settings.json");
         const contents = await readTextFile(path);
         settingsJson = JSON.parse(contents);
-    } catch(e: any) {
-        console.error(`Settings Load Failed: ${e}`);
+        info("Settings load successful");
+    } 
+    catch(e: any) {
+        error(`Settings Load Failed: ${e}`);
     }
 }
 
@@ -21,22 +24,31 @@ export async function getValue(key: string): Promise<any> {
         loaded = true;
     }
     try {
+        info(`Attempting to get key ${key}`)
         const value = settingsJson[key];
         return value;
     } catch(e: any) {
-        console.error(`Error while trying to get key ${key}: ${e}`)
+        error(`Error while trying to get key ${key}: ${e}`)
     }
 }
 
 export function setValue(key: string, value: any) {
+    info(`Setting value of key ${key} to ${value}`);
     settingsJson[key] = value;
-    debounce(async() => await saveJsonData(), 500);
+    saveJsonData(); // No need to await this function to be complete, since it's just saving the new settings to a json file
 }
 
 async function saveJsonData() {
-    const content = JSON.stringify(settingsJson);
-    const file = await resolveResource("resources/settings.json");
+    try {
+        info("Saving Json Data");
+        const content = JSON.stringify(settingsJson);
+        const file = await resolveResource("resources/settings.json");
 
-    await writeTextFile(file, content);
-    await loadSettings(); // Reload settings json
+        await writeTextFile(file, content);
+        info("Json Data Save Successful.");
+        await loadSettings(); // Reload settings json
+    }
+    catch(e: any) {
+        error(`Error whilst saving settings data: ${e}`)
+    }
 }
